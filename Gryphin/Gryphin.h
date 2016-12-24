42c5e806b9f8a2d8c12875f90a09a5d76b5da708
Dima Bart <dima.bart01@gmail.com>
2016-12-24 10:20:39 -0500

    Initial Commit

---
+++ Gryphin/Gryphin.h
@@ -0,0 +1,19 @@
+//
+//  Gryphin.h
+//  Gryphin
+//
+//  Created by Dima Bart on 2016-12-24.
+//  Copyright © 2016 Dima Bart. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Gryphin.
+FOUNDATION_EXPORT double GryphinVersionNumber;
+
+//! Project version string for Gryphin.
+FOUNDATION_EXPORT const unsigned char GryphinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gryphin/PublicHeader.h>
+
+
