780f1ac50e4d1bdffacba0dd52c88c543e0c7c53
Dima Bart <dima.bart01@gmail.com>
2016-12-28 10:15:40 -0500

    Add missing fields to Object.

---
+++ Gryphin/Schema.EnumValue.swift
@@ -0,0 +1,29 @@
+//
+//  Schema.EnumValue.swift
+//  Gryphin
+//
+//  Created by Dima Bart on 2016-12-28.
+//  Copyright © 2016 Dima Bart. All rights reserved.
+//
+
+import Foundation
+
+extension Schema {
+    final class EnumValue: JsonCreatable {
+        
+        let name:              String
+        let description:       String?
+        let isDeprecated:      Bool
+        let deprecationReason: String?
+        
+        // ----------------------------------
+        //  MARK: - Init -
+        //
+        init(json: JSON) {
+            self.name              = json["name"]              as! String
+            self.description       = json["description"]       as? String
+            self.isDeprecated      = json["isDeprecated"]      as? Bool ?? false
+            self.deprecationReason = json["deprecationReason"] as? String
+        }
+    }
+}
--- Gryphin/Schema.Object.swift
+++ Gryphin/Schema.Object.swift
@@ -11,20 +11,27 @@ import Foundation
 extension Schema {
     final class Object: JsonCreatable {
         
-        let kind:        Kind
-        let name:        String
-        let description: String?
-        let fields:      [Field]?
+        let kind:          Kind
+        let name:          String
+        let description:   String?
+        let fields:        [Field]?
+//        let inputFields:   [Field]?
+        let interfaces:    [ObjectType]?
+        let enumValues:    [EnumValue]?
+        let possibleTypes: [ObjectType]?
         
         // ----------------------------------
         //  MARK: - Init -
         //
         init(json: JSON) {
-            self.kind        = Kind(string: json["kind"] as! String)
-            self.name        = json["name"]              as! String
-            self.description = json["description"]       as? String
+            self.kind          = Kind(string: json["kind"] as! String)
+            self.name          = json["name"]              as! String
+            self.description   = json["description"]       as? String
             
-            self.fields      = Field.collectionWith(optionalJson: json["fields"] as? [JSON])
+            self.fields        = Field.collectionWith(optionalJson:      json["fields"]        as? [JSON])
+            self.interfaces    = ObjectType.collectionWith(optionalJson: json["interfaces"]    as? [JSON])
+            self.enumValues    = EnumValue.collectionWith(optionalJson:  json["enumValues"]    as? [JSON])
+            self.possibleTypes = ObjectType.collectionWith(optionalJson: json["possibleTypes"] as? [JSON])
         }
     }
 }
