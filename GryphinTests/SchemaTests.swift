679a749d2903cd700d79823172e71384bc906306
Dima Bart <dima.bart01@gmail.com>
2017-01-17 16:25:24 -0500

    Update generated schema file names for faster testing.

--- GryphinTests/SchemaTests.swift
+++ GryphinTests/SchemaTests.swift
@@ -22,114 +22,114 @@ class SchemaTests: XCTestCase {
         let schema    = result.schema.stringRepresentation
         let models    = result.models.stringRepresentation
         
-        let schemaPath = URL(fileURLWithPath: "/Users/dbart/Desktop/Schema.swift")
-        let modelsPath = URL(fileURLWithPath: "/Users/dbart/Desktop/Models.swift")
+        let schemaPath = URL(fileURLWithPath: "/Users/dbart/Desktop/APISchema.swift")
+        let modelsPath = URL(fileURLWithPath: "/Users/dbart/Desktop/APIModels.swift")
         
         try! schema.write(to: schemaPath, atomically: true, encoding: .utf8)
         try! models.write(to: modelsPath, atomically: true, encoding: .utf8)
     }
     
-//    func testMutation() {
-//        let project = CreateProjectInput(clientMutationId: "some123", ownerId: "owner123", name: "Wobbly Bear", body: "Nothing to pu here")
-//        
-//        let mutation = Mutation { $0
-//            .createProject(input: project) { $0
-//                .clientMutationId
-//                .project { _ = $0
-//                    .id
-//                    .name
-//                    .body
-//                    .url
-//                }
-//            }
-//        }
-//        
-//        print(mutation._stringRepresentation)
-//    }
-//    
-//    func testQuery() {
-//        let order = RepositoryOrder(field: .createdAt, direction: .asc)
-//        let query = Query { $0
-//            .repository(owner: "dbart01", name: "someName") { $0
-//                .owner { $0
-//                    .fragmentOnOrganization { _ = $0
-//                        .id
-//                        .login
-//                    }
-//                    .fragmentOnUser { _ = $0
-//                        .id
-//                        .login
-//                        .isViewer
-//                        .isEmployee
-//                    }
-//                    .repositories(first: 15, orderBy: order) { $0
-//                        .edges { $0
-//                            .node { _ = $0
-//                                .createdAt
-//                                .name
-//                                .description
-//                            }
-//                        }
-//                    }
-//                }
-//                .alias("ownerAlias").owner { _ = $0
-//                    .id
-//                    .login
-//                }
-//                .alias("issueAlias").issues(first: 20) { $0
-//                    .edges { $0
-//                        .node { _ = $0
-//                            .body
-//                            .createdAt
-//                            .id
-//                        }
-//                    }
-//                }
-//                .ref(qualifiedName: "/ref/branch/master") { $0
-//                    .associatedPullRequests(first: 20, states: [.open, .closed]) { $0
-//                        .edges { $0
-//                            .node { _ = $0
-//                                .bodyHTML
-//                                .body
-//                            }
-//                        }
-//                    }
-//                }
-//                .issues(first: 20) { $0
-//                    .totalCount
-//                    .edges { $0
-//                        .node { $0
-//                            .assignees(first: 20) { $0
-//                                .edges { $0
-//                                    .node { _ = $0
-//                                        .name
-//                                        .id
-//                                        .isViewer
-//                                        .isEmployee
-//                                        .isSiteAdmin
-//                                        .isBountyHunter
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//                .forks(first: 20) { $0
-//                    .edges { $0
-//                        .cursor
-//                        .node { _ = $0
-//                            .createdAt
-//                            .description
-//                            .descriptionHTML
-//                            .id
-//                            .name
-//                            .homepageURL
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    
-//        print(query._stringRepresentation)
-//    }
+    func testMutation() {
+        let project = CreateProjectInput(clientMutationId: "some123", ownerId: "owner123", name: "Wobbly Bear", body: "Nothing to pu here")
+        
+        let mutation = Mutation { $0
+            .createProject(input: project) { $0
+                .clientMutationId
+                .project { _ = $0
+                    .id
+                    .name
+                    .body
+                    .url
+                }
+            }
+        }
+        
+        print(mutation._stringRepresentation)
+    }
+    
+    func testQuery() {
+        let order = RepositoryOrder(field: .createdAt, direction: .asc)
+        let query = Query { $0
+            .repository(owner: "dbart01", name: "someName") { $0
+                .owner { $0
+                    .fragmentOnOrganization { _ = $0
+                        .id
+                        .login
+                    }
+                    .fragmentOnUser { _ = $0
+                        .id
+                        .login
+                        .isViewer
+                        .isEmployee
+                    }
+                    .repositories(first: 15, orderBy: order) { $0
+                        .edges { $0
+                            .node { _ = $0
+                                .createdAt
+                                .name
+                                .description
+                            }
+                        }
+                    }
+                }
+                .alias("ownerAlias").owner { _ = $0
+                    .id
+                    .login
+                }
+                .alias("issueAlias").issues(first: 20) { $0
+                    .edges { $0
+                        .node { _ = $0
+                            .body
+                            .createdAt
+                            .id
+                        }
+                    }
+                }
+                .ref(qualifiedName: "/ref/branch/master") { $0
+                    .associatedPullRequests(first: 20, states: [.open, .closed]) { $0
+                        .edges { $0
+                            .node { _ = $0
+                                .bodyHTML
+                                .body
+                            }
+                        }
+                    }
+                }
+                .issues(first: 20) { $0
+                    .totalCount
+                    .edges { $0
+                        .node { $0
+                            .assignees(first: 20) { $0
+                                .edges { $0
+                                    .node { _ = $0
+                                        .name
+                                        .id
+                                        .isViewer
+                                        .isEmployee
+                                        .isSiteAdmin
+                                        .isBountyHunter
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                .forks(first: 20) { $0
+                    .edges { $0
+                        .cursor
+                        .node { _ = $0
+                            .createdAt
+                            .description
+                            .descriptionHTML
+                            .id
+                            .name
+                            .homepageURL
+                        }
+                    }
+                }
+            }
+        }
+    
+        print(query._stringRepresentation)
+    }
 }
