b5da566f71e5eb1cd4a2c0dc11884c7e08fa5483
Dima Bart <dima.bart01@gmail.com>
2017-01-29 12:23:47 -0500

    Add Swift.Visibility tests.

---
+++ GryphinTests/Swift.VisibilityTests.swift
@@ -0,0 +1,25 @@
+//
+//  Swift.VisibilityTests.swift
+//  Gryphin
+//
+//  Created by Dima Bart on 2017-01-29.
+//  Copyright © 2017 Dima Bart. All rights reserved.
+//
+
+import XCTest
+@testable import Gryphin
+
+class SwiftVisibilityTests: XCTestCase {
+    
+    // ----------------------------------
+    //  MARK: - Init -
+    //
+    func testValues() {
+        XCTAssertEqual(Swift.Visibility.private.rawValue,     "private")
+        XCTAssertEqual(Swift.Visibility.fileprivate.rawValue, "fileprivate")
+        XCTAssertEqual(Swift.Visibility.internal.rawValue,    "internal")
+        XCTAssertEqual(Swift.Visibility.public.rawValue,      "public")
+        XCTAssertEqual(Swift.Visibility.open.rawValue,        "open")
+        XCTAssertEqual(Swift.Visibility.none.rawValue,        "")
+    }
+}
