709887acc8c1c87e09f346284739e2a26a69a5c2
Dima Bart <dima.bart01@gmail.com>
2017-01-31 16:16:20 -0500

    Add Swift.Alias tests.

---
+++ GryphinTests/Swift.AliasTests.swift
@@ -0,0 +1,39 @@
+//
+//  Swift.AliasTests.swift
+//  Gryphin
+//
+//  Created by Dima Bart on 2017-01-31.
+//  Copyright © 2017 Dima Bart. All rights reserved.
+//
+
+import XCTest
+@testable import Gryphin
+
+class SwiftAliasTests: XCTestCase {
+
+    // ----------------------------------
+    //  MARK: - Init -
+    //
+    func testInit() {
+        let alias = Swift.Alias(name: "SuperString", forType: "String")
+        
+        XCTAssertNil(alias.parent)
+        XCTAssertEqual(alias.name,    "SuperString")
+        XCTAssertEqual(alias.forType, "String")
+    }
+    
+    func testStringRepresentation() {
+        let alias = Swift.Alias(name: "SuperString", forType: "String")
+        
+        let container    = Swift.Container()
+        let subcontainer = Swift.Container()
+        
+        container.add(child: subcontainer)
+        subcontainer.add(child: alias)
+        
+        XCTAssertEqual(alias.stringRepresentation, "" ~
+            "    typealias SuperString = String" ~
+            ""
+        )
+    }
+}
